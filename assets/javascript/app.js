8dc6d2ca48383797d05d81ab2f84f6998d5a3e54
elspetha5 <elspetha@gmail.com>
2018-03-03 11:24:18 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,59 @@
 $(document).ready(function () {
 
     var time = 30;
-    var count = setInterval(timer, 1000);
+    var count;
 
+    var qNumber = 1;
+    var cNumber = 1;
+    var correct = $("#correct" + cNumber);
+    var rightAnswer = correct.text();
+
+    
+    var userGuess = $(".guess").click(function() {
+        $(this).attr("id");
+    });
+    console.log("userGuess:", userGuess);
+
+    function answer() {
+        $("#q" + qNumber).hide();
+         qNumber++;
+        // if (userGuess === correct) {
+        //  $("guess").text("That is correct!");
+        //} else {$("guess).text(""Sorry, wrong answer.")};
+        $("#right").text(rightAnswer)
+        cNumber++;
+        $("#answer").appendTo("#text").removeAttr("hidden");
+    };
+    
     function timer() {
         time--;
         $("#time").text(time);
         if (time <= 0) {
             clearInterval(count);
-            //move on to next question
+            answer();
         } else if (time <= 5) {
             $("#time").removeClass("pink").addClass("red");
         } else if (time <= 10) {
             $("#time").addClass("pink");
         }
-    }
-
+    };
 
     // How do we start the game?
     $("#start").on("click", function () {
         $("#start").hide();
         // timer appears on screen, set at 30 seconds
         $("#timer").removeAttr("hidden");
+        count = setInterval(timer, 1000);
         count
         // text on screen changes to first question
-        $("#q1").appendTo("#questions").removeAttr("hidden");
+        $("#q" + qNumber).appendTo("#text").removeAttr("hidden");
     })
 
 
     // applies only to current question, not whole game
 
     // What happens when we click an anwer?
-    
+
         // timer stops
         // question disappears
         // screen tells user if they were wrong or right
